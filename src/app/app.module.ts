ee9442e46edf50d5b844beb03ac9df1b131aed1c
lucagobbi <lucasghegobbi@gmail.com>
2022-11-24 00:06:26 +0100

    added material, partially prepared form groups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CvFormComponent } from './cv-form/cv-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import { MatNativeDateModule, MatOptionModule} from "@angular/material/core";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatStepperModule} from "@angular/material/stepper";
 
 @NgModule({
   declarations: [
@@ -10,7 +18,16 @@ import { CvFormComponent } from './cv-form/cv-form.component';
     CvFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cv-form/cv-form.component.ts
+++ src/app/cv-form/cv-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder} from "@angular/forms";
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-cv-form',
@@ -8,11 +8,80 @@ import {FormBuilder} from "@angular/forms";
 })
 export class CvFormComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) { }
+  userFormGroup: FormGroup;
+  educationFormGroup: FormGroup;
+  skillsFormGroup: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.userFormGroup = this.buildUserFromGroup();
+    this.educationFormGroup = this.buildEducationFormGroup()
+    this.skillsFormGroup = this.buildSkillsFormGroup();
   }
+
+  ngOnInit(): void {}
 
   downloadCV() {}
 
+  buildUserFromGroup(): FormGroup {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      email: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      address: ['', Validators.required],
+      intro: ['', Validators.required]
+    })
+  }
+
+  buildEducationFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      educationItems: this.formBuilder.array([
+        this.formBuilder.group({
+          dateOfStart: ['', Validators.required],
+          dateOfEnd: ['', Validators.required],
+          location: ['', Validators.required],
+          description: ['', Validators.required],
+          field: ['', Validators.required],
+          title: ['', Validators.required],
+          graduation: ['', Validators.required]
+        })
+      ]),
+    })
+  }
+
+  buildSkillsFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      skills: this.formBuilder.array([
+        this.formBuilder.group({
+          title: ['', Validators.required],
+          level: ['', Validators.required]
+        })
+      ])
+    })
+  }
+
+
+
+  // experienceItems: this.formBuilder.array([
+  //                                           this.formBuilder.group({
+  //     dateOfStart: ['', Validators.required],
+  //     dateOfEnd: ['', Validators.required],
+  //     location: ['', Validators.required],
+  //     description: ['', Validators.required],
+  //     appliedSkills: this.formBuilder.array([
+  //     this.formBuilder.group({
+  //       title: ['', Validators.required],
+  //       level: ['', Validators.required]
+  //     })
+  //   ])
+  //                                           })
+  //                                         ]),
+  //                                ]),
+  // contacts: this.formBuilder.array([
+  //                                    this.formBuilder.group({
+  //     social: ['', Validators.required],
+  //     link: ['', Validators.required]
+  //   })
+  //                                  ])
 }
